63b195af3e2cd6e89d23582696d25d3f0224dc97
Entreprenuer Zeeshan <dr5269139@gmail.com>
2024-04-17 22:52:52 +0500

    Second Typescript Lecture of class

---
+++ app.js
@@ -0,0 +1,6 @@
+import slogin from "./Student";
+import tlogin from "./Teacher";
+let teacher = new tlogin();
+console.log(teacher.data);
+let student = new slogin();
+console.log(student.data);
---
+++ app.ts
@@ -0,0 +1,9 @@
+import slogin from "./Student";
+import tlogin from "./Teacher";
+
+let teacher = new tlogin();
+console.log(teacher.data);
+
+
+let student = new slogin();
+console.log(student.data);
